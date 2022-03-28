11814279fc5f0be533fdc1e65297df75b7083cd4
LoKi <sand5088@gmail.com>
2022-03-28 14:53:59 +0700

    scaffold pool model and update UI action

---
+++ app/views/pools/_pool.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! pool, :id, :project, :round, :platform, :amount, :price, :vesting, :launchdate, :profit, :created_at, :updated_at
+json.url pool_url(pool, format: :json)
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 
 pin_all_from "app/javascript/custom", under: "custom"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.1.5/lib/assets/compiled/rails-ujs.js"
