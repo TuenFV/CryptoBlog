fca266e790400933aeb22e8ac18817807fe18d2c
-LoKi- <44358997+TuenFV@users.noreply.github.com>
2022-04-03 17:19:13 +0700

    Validate models (#9)
    * vesting model for each pool with association
    
    * how to use mailer
    
    * vesting email reminder
    
    * send email when vesting date via action mailer and task
    
    * update UI 2
    
    * optimal display
    
    * validates pool and vesting models

--- app/models/vesting.rb
+++ app/models/vesting.rb
@@ -1,3 +1,6 @@
 class Vesting < ApplicationRecord
   belongs_to :pool
+
+  validates :rate, presence: true, numericality: { greater_than: 0}
+  validates :soldtoken, :return, numericality: { greater_than: 0}, allow_nil: true
 end
