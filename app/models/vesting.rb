9e6d504a2a523bdc873ea17f6b98fc48aca996df
LoKi <sand5088@gmail.com>
2022-04-04 23:51:47 +0700

    add default date and rate of new vesting

--- app/models/vesting.rb
+++ app/models/vesting.rb
@@ -1,6 +1,7 @@
 class Vesting < ApplicationRecord
   belongs_to :pool
 
+  validates :date, uniqueness: { scope: :pool_id }
   validates :rate, presence: true, numericality: { greater_than: 0}
   validates :soldtoken, :return, numericality: { greater_than: 0}, allow_nil: true
 end
