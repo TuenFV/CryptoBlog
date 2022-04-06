7a5ccfd886d29376fa7e4fb475569937f528b885
LoKi <sand5088@gmail.com>
2022-04-07 01:36:33 +0700

    add news from api-cryptocompare.com

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    # Grab News
+    @url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
+    @uri = URI(@url)
+    @respone = Net::HTTP.get(@uri)
+    @news = JSON.parse(@respone)
+
+    #Grab Prices
   end
 end
