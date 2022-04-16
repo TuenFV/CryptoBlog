22ce1b69633c52b2a16c1dda00b7d7c62e4337bd
LoKi <sand5088@gmail.com>
2022-04-17 01:22:00 +0700

    add total amount and profit

--- app/helpers/pools_helper.rb
+++ app/helpers/pools_helper.rb
@@ -2,9 +2,17 @@ module PoolsHelper
   def pool_profit(pool)
     profit = 0
     @pool = pool
-    pool.vestings.each do |vesting|
+    pool.vestings.find_each do |vesting|
       profit += vesting.return unless vesting.return.nil?
     end
     pool.amount.nil? ? 0 : (profit - pool.amount).round(0)
+  end
+
+  def total_profit(user)
+    total = 0
+    user.pools.each do |pool|
+      total += pool_profit(pool)
+    end
+    return total
   end
 end
