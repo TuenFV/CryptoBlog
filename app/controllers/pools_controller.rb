cdd861142a400485ff46ce0435279e9a50149f1d
LoKi <sand5088@gmail.com>
2022-04-17 22:40:19 +0700

    add validate total vesting rate

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -5,7 +5,7 @@ class PoolsController < ApplicationController
 
   # GET /pools or /pools.json
   def index
-    @pools = current_user.pools.sort.reverse!
+    @pools = current_user.pools.order(launchdate: :desc)
   end
 
   # GET /pools/1 or /pools/1.json
--- app/models/vesting.rb
+++ app/models/vesting.rb
@@ -1,7 +1,23 @@
+class TotalRateValidator < ActiveModel::Validator
+  def validate(vesting)
+    if vesting.id.nil?
+      if (vesting.pool.vestings.sum(:rate) + vesting.rate) > 100
+        vesting.errors.add :base, "Total vesting rate is over 100%"
+      end
+    else
+    # When commit update, we have rate of old value: Vesting.find(vesting.id).rate and new value: vesting.rate
+      if (vesting.pool.vestings.sum(:rate) - Vesting.find(vesting.id).rate + vesting.rate ) > 100
+        vesting.errors.add :base, "Total vesting rate is over 100%"
+      end
+    end
+  end
+end
+
 class Vesting < ApplicationRecord
   belongs_to :pool
 
   validates :date, uniqueness: { scope: :pool_id }
   validates :rate, presence: true, numericality: { greater_than: 0}
   validates :soldtoken, :return, numericality: { greater_than: 0}, allow_nil: true
+  validates_with TotalRateValidator
 end
